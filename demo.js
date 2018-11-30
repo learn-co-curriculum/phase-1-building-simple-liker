ab1c44d28eb7ae5fd2bc48dde35b8f9359fbab3e
Steven G. Harms <steven.harms@flatironschool.com>
2018-11-30 14:15:51 -0500

    Update demo.js to have scaffolding server

--- demo.js
+++ demo.js
@@ -1,1 +1,19 @@
 // JavaScript code goes here!
+
+
+
+// Your code goes above this line!
+
+//------------------------------------------------------------------------------
+// Ignore after this point. Used only for demo purposes
+//------------------------------------------------------------------------------
+
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    console.log("The fake server at mimicServer called saw a request!");
+    setTimeout(function() {
+      if (config.forceFailure) reject("Failure was forced");
+      resolve("Pretend remote server notified of action!");
+    }, 300);
+  });
+}
