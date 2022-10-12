1e77629d85879f866e38612865179f894e964c23
Vladyslav <vkadyrov@protonmail.com>
2022-10-11 22:37:53 -0400

    commit tree bf06989037a016ea3ec47514030b88b6ee2c6606

--- main.js
+++ main.js
@@ -4,6 +4,29 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const heartSelector = document.querySelectorAll('.like-glyph')
+// heartSelector.addEventListener('click', likeButton);
+
+function likeButton(e) {
+    const heart = e.target
+    mimicServerCall()
+    .then(() => {
+      heart.innerText = FULL_HEART
+    })
+    .catch(() => {
+      document.querySelector('#modal').classList.remove('hidden')
+      setTimeout(addHidden(), 3000)
+    })
+
+    function addHidden() {
+      const errorMessage = document.querySelector('#modal')
+      errorMessage.classList.add('hidden')
+    };
+
+    for (const glyph of heartSelector) {
+      glyph.addEventListener('click', likeButton)
+    }
+};
 
 
 
